437ea02d8f01728c4ed06f60abaa362cb9d8ecea
“Juliana” <ju.uemura@gmail.com>
2016-03-23 08:17:30 +0000

    changed collections fk to :user instead of :users

--- db/migrate/20160311034952_create_collections.rb
+++ db/migrate/20160311034952_create_collections.rb
@@ -1,7 +1,7 @@
 class CreateCollections < ActiveRecord::Migration
   def change
     create_table :collections do |t|
-      t.references :users, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.boolean :isPublished
       t.string :name
       t.timestamps null: false
