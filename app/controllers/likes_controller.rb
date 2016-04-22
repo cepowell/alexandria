5b01e2076da8945fb26b9d50f65b96bb63d10d1c
Cait <cpowell@mills.edu>
2016-04-22 08:49:22 +0000

    Updating views for documents and collections, limiting users to one like per document

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,13 @@
 class LikesController < ApplicationController
     
     def new 
-        like = Like.create(params[:like])
-        like.save
-        redirect_to request.referrer
+        document = Document.find(params[:like][:document_id])
+        if !Like.where(user_id: session[:user_id], document_id: document.id).exists?
+            like = Like.create(params[:like])
+            like.save
+            redirect_to request.referrer
+        else
+            redirect_to request.referrer
+        end
     end
 end
